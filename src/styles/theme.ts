ed4c5d67c35b2de4a9c17f290e6a7bf76427f94e
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-08-05 12:20:54 -0300

    tela de sign criada e estilizada

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,22 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
     colors: {
-
+        gray: {
+            "900": "#181B23",
+            "800": "#1F2029",
+            "700": "#353646",
+            "600": "#4B4D63",
+            "500": "#616480",
+            "400": "#797D9A",
+            "300": "#9699B0",
+            "200": "#B3B5C6",
+            "100": "#D1D2DC",
+            "50": "#EEEEF2"
+        }
+    },
+    fonts: {
+        heading: "Roboto",
+        body: "Roboto"
     },
     styles: {
         global: {
@@ -12,4 +27,4 @@ export const theme = extendTheme({
             }
         }
     }
-});
+})
